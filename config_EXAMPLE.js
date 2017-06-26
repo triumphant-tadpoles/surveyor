d9b3f6ded78649ef1be016853c2a44c239da73a7
David Oh <davidjohis@gmail.com>
2017-06-26 10:11:52 -0700

    successfully set up server and communication with heroku postgres

---
+++ config_EXAMPLE.js
@@ -0,0 +1,5 @@
+var config = {
+  postgresUrl: `GET URL FROM THIS PROJECT'S ACCOUNT`
+};
+
+module.exports = config;
---
+++ database-postgresql/index.js
@@ -0,0 +1,26 @@
+var pg = require('pg');
+var config = require('../config.js');
+console.log(config);
+// 'process.env.DATABASE_URL' is avaialable in Heroku's environment, otherwise, obtain postgresUrl from this project's DB account.
+var postgresUrl = (process.env.DATABASE_URL || config.postgresUrl);
+
+
+// REFER TO https://stackoverflow.com/questions/8484404/what-is-the-proper-way-to-use-the-node-js-postgresql-module
+// ALSO, https://stackoverflow.com/questions/8484404/what-is-the-proper-way-to-use-the-node-js-postgresql-module
+// ADDING 'pg.defaults.ssl = true;' IS IMPORTANT, OTHERWISE THERE WILL BE AN ERROR IN CONNECTION TO DB!!!
+
+pg.defaults.ssl = true;
+module.exports = {
+  query: function(text, cb) {
+    pg.connect(postgresUrl, function(error, client) {
+      if (error) {
+        throw error;
+      }
+      client
+        .query(text)
+        .then(results => {
+          cb(results);
+        });
+    });
+  }
+};
