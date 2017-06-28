58c9163b5b2b2d1dfddd051b35072fad9557e380
codepadma <padma.govindarajalu@gmail.com>
2017-06-28 16:19:23 -0700

    removed the publisher key

--- server/externals/indeed.js
+++ server/externals/indeed.js
@@ -23,7 +23,7 @@ module.exports.indeed = (req, res, next) => {
 }
 
 let indeedFetch = (req, res, next, city, state, query) => {
-  fetch(`http://api.indeed.com/ads/apisearch?format=json&v=2&publisher=8865882670516899&q=${query}&l=${city}%2C+${state}&userAgent=${req.get('user-agent')}&limit=100&fromage=10&radius=100`, {
+  fetch(`http://api.indeed.com/ads/apisearch?format=json&v=2&publisher=${indeed}&q=${query}&l=${city}%2C+${state}&userAgent=${req.get('user-agent')}&limit=100&fromage=10&radius=100`, {
     method: 'GET'
   }).then((response, error) =>{
     if (error) throw error;
