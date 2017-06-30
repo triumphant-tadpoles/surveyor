4a94f4f9994af7e7caf4ea6def4cc5067986ddfb
Alexander Schnapp <alexanderschnapp@gmail.com>
2017-06-30 12:26:50 -0700

    Changed login bug

--- react-client/src/components/Login.jsx
+++ react-client/src/components/Login.jsx
@@ -10,7 +10,7 @@ class Login extends React.Component {
     return(
       <div>
         <FacebookLogin
-          appId={process.env.FACEBOOKDEVID}
+          appId={`${process.env.FACEBOOKDEVID}`}
           fields='name, email, picture'
           callback={this.props.onLogin}
         />
