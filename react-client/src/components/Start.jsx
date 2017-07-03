24ee1a791d08aee2a5552f60fe1d83954e9d799e
codepadma <padma.govindarajalu@gmail.com>
2017-07-03 13:44:42 -0700

    modified error message

--- react-client/src/components/Start.jsx
+++ react-client/src/components/Start.jsx
@@ -8,7 +8,7 @@ class Start extends React.Component {
     return (
       <div className="dropzone">
         {this.props.errMsg.length > 0
-         ? <h3> Error analysing Resume! Please try again later</h3>
+         ? <h3> There was an error analyzing your resume. Please try again.</h3>
          : null
         }
         <h3>Drag Resume Here</h3>
